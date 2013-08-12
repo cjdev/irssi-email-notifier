eaf019b44214aef62d007e40ea9321004321ecc0
David Ron <david.m.ron@gmail.com>
2013-08-12 14:01:08 -0700

    Initial Commit

--- fnotify.pl
+++ fnotify.pl
@@ -7,16 +7,18 @@ use vars qw($VERSION %IRSSI);
 use Irssi;
 $VERSION = '0.0.3';
 %IRSSI = (
-    authors     => 'Thorsten Leemhuis',
-    contact     => 'fedora@leemhuis.info',
+    authors     => 'David Ron',
+    contact     => 'dron@cj.com',
     name        => 'fnotify',
     description => 'Write a notification to a file that shows who is talking to you in which channel.',
-    url         => 'http://www.leemhuis.info/files/fnotify/',
-    license     => 'GNU General Public License',
-    changed     => '$Date: 2007-01-13 12:00:00 +0100 (Sat, 13 Jan 2007) $'
+    url         => 'https://github.com/cjdev/irssi-email-notifier',
+    license     => 'GNU General Public License'
 );
 
 #--------------------------------------------------------------------
+# Based on Thorsten Leemhuis's fnotify.pl
+# http://blog.kintoandar.com/2011/06/irssi-irc-mention-notify-by-email-using.html
+#
 # In parts based on knotify.pl 0.1.1 by Hugo Haas
 # http://larve.net/people/hugo/2005/01/knotify.pl
 # which is based on osd.pl 0.3.3 by Jeroen Coekaerts, Koenraad Heijlen
@@ -25,13 +27,12 @@ $VERSION = '0.0.3';
 # Other parts based on notify.pl from Luke Macken
 # http://fedora.feedjack.org/user/918/
 #
-# ssmtp email notification support added by Joel Bastos
-# http://kintoandar.blogspot.com
+# David Ron added support for mail and removed the race condition.
 #
 #--------------------------------------------------------------------
 
-# Please set the variables (don't forget to escape "\" the "@" symbol like the example)
-my $EMAIL = "david.m.ron\@gmail.com";
+# Please set the variable (don't forget to escape "\" the "@" symbol like the example)
+my $EMAIL = "YOUR_ADDRESS\@gmail.com";
 
 #--------------------------------------------------------------------
 # Private message parsing
@@ -58,14 +59,14 @@ sub hilight {
 #--------------------------------------------------------------------
 
 sub filewrite {
-        my ($text) = @_;
-        # FIXME: there is probably a better way to get the irssi-dir...
+    my ($text) = @_;
+    # FIXME: there is probably a better way to get the irssi-dir...
 	my $date = `date`;
 	my $random = my $random_number = int(rand(10000));
 	my $fileName = "$ENV{HOME}/.irssi/notifications/fnotify$random";
-        open(FILE,">>$fileName");
-        print FILE $date . $text . "\n\n";
-        close (FILE);
+    open(FILE,">>$fileName");
+    print FILE $date . $text . "\n\n";
+    close (FILE);
 	my $mail=`cat $fileName | mail -s "IRSSI Notification" $EMAIL`;
 }
 
@@ -75,3 +76,4 @@ sub filewrite {
 
 Irssi::signal_add_last("message private", "priv_msg");
 Irssi::signal_add_last("print text", "hilight"); 
+
